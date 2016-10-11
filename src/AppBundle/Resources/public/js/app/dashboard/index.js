9e27c1e814a5ee99119231a0f69fdc4ae727ae08
jacquesdufour <jacques-i-class@hotmail.fr>
2016-10-11 15:14:51 +0100

    dashboard integration

---
+++ src/AppBundle/Resources/public/js/app/dashboard/index.js
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Benagro package.
+ *
+ * (c) Jacques Adjahoungbo <jtocson@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+/**
+ * New product page
+ *
+ * @version 1.0 [11 Octobre 2016]
+ * @author Jacques Adjahoungbo <jtocson@gmail.com>
+ */
+
+define(
+    [
+    'jquery'
+],
+    function($) {
+        'use strict';
+
+        var toggle = true;
+        $(".sidebar-icon").click(function() {                
+            if (toggle)
+            {
+                $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+                $("#menu span").css({"position":"absolute"});
+            }
+            else
+            {
+                $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+                setTimeout(function() {
+                $("#menu span").css({"position":"relative"});
+                }, 400);
+            }
+            toggle = !toggle;
+        });
+    }
+);
+
