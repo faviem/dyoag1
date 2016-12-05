2e0ee2d53e9960457fe1c20224d8d11d4377e18e
faviem <fvemile2012@gmail.com>
2016-12-05 18:23:24 +0100

    05-12-2016 version 3

---
+++ src/AppBundle/Controller/MoncompteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+//use AppBundle\Form\ContactType;
+use AppBundle\Form\ContactHandler;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+
+/**
+ * moncompte controller.
+ *
+ * @Route("/dashboard")
+ */
+class MoncompteController extends Controller
+{
+    /**
+     * @Route("/moncompte", name="dasboard_moncompteindex")
+     * @Template()
+     * @Cache(smaxage="600", public="true")
+     */
+    public function indexAction(Request $request)
+    {        
+        return $this->render('dashboard/moncompte/index.html.twig');
+    }    
+    
+}
--- src/AppBundle/Resources/public/js/app/dashboard/index.js
+++ src/AppBundle/Resources/public/js/app/dashboard/index.js
@@ -16,28 +16,26 @@
  */
 
 define(
-    [
-    'jquery'
-],
-    function($) {
-        'use strict';
+        [
+            'jquery',
+            // 'bootstrap',
+            'datatable'
+        ],
+        function ($, DataTable) {
+            'use strict';
+            console.log('OK');
+            $('#dataTables-example').DataTable();
 
-        var toggle = true;
-        $(".sidebar-icon").click(function() {                
-            if (toggle)
-            {
-                $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
-                $("#menu span").css({"position":"absolute"});
-            }
-            else
-            {
-                $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
-                setTimeout(function() {
-                $("#menu span").css({"position":"relative"});
-                }, 400);
-            }
-            toggle = !toggle;
-        });
-    }
+            /**
+             * @module       Tabs
+             * @description  Bootstrap tabs
+             */
+
+//            $('#myTabs a').click(function (e) {
+//                e.preventDefault()
+//                $(this).tab('show')
+//            });
+
+        }
 );
 
